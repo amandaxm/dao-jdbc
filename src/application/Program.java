f15df76efd3473743114b4176a4996254425c312
Amanda Xavier Mariano <amandaxaviermariano10@gmail.com>
2020-05-24 17:47:23 -0300

    Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,17 @@ public class Program {
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
-		System.out.println("\n=== teste  4; seller findAll===");
+		System.out.println("\n=== teste  4; seller insert===");
 		Seller newSeller = new Seller(null,"Greg","greg@gmail.com", new Date(),4000.00,department);
 		sellerDao.insert(newSeller);
-		System.out.println("Insert"+newSeller.getId());
+		System.out.println("Insert "+newSeller.getId());
+	
+		System.out.println("\n=== teste  5; seller insert===");
+		seller = sellerDao.findbyId(1);
+		seller.setName("Matrha");
+		sellerDao.update(seller);
+		System.out.println("Update completed ");
+
 	}
 
 }

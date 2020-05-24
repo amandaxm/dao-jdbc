d35efe3b98417b1136623f174bbee223d747d3e9
Amanda Xavier Mariano <amandaxaviermariano10@gmail.com>
2020-05-24 11:34:06 -0300

    Reusando a instanciacao

--- src/model/dao/imple/SellerDaoJDBC.java
+++ src/model/dao/imple/SellerDaoJDBC.java
@@ -50,17 +50,8 @@ public class SellerDaoJDBC implements SellerDao {
 			rs = st.executeQuery();
 			//vem como tabela
 			if(rs.next()) {
-				Department dep = new Department();
-				//setar e pegar o nome que veio da tabela
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
+				Department dep = instantiateDepartment(rs);
+				Seller obj = instantiateSeller(rs,dep);
 				return obj;
 			}return null;//se veio algum resultado
 		}catch(SQLException e){
@@ -72,6 +63,23 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 	}
 
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj=new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep= new Department();
+			//setar e pegar o nome que veio da tabela
+			dep.setId(rs.getInt("DepartmentId"));
+			dep.setName(rs.getString("DepName"));
+			return dep;
+	}
 	@Override
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub

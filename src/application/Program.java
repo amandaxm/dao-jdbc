bbd3b4ebf65a3ab0e8170f2b6694dc9fb8261786
myusername <amandaxaviermariano10@gmail.com>
2020-05-24 10:28:23 -0300

    Classe Departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
-
+//sistema de vendedores que tem departamentos
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Department obj= new Department(1,"Books");
+		System.out.println(obj);
+	
 	}
 
 }
